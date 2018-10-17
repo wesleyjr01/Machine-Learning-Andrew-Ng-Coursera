bfd7832bf7b27c1b53b12c10e043d7e600e4a3d6
Wesley André Bortolozo Júnior <wesleyjr0101@gmail.com>
2018-10-17 19:57:18 -0300

    Add files via upload

---
+++ Week 8 - K-Means and PCA/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -0,0 +1,27 @@
+function idx = findClosestCentroids(X, centroids)
+%FINDCLOSESTCENTROIDS computes the centroid memberships for every example
+%   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
+%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   vector of centroid assignments (i.e. each entry in range [1..K])
+%
+
+% Set K
+K = size(centroids, 1);
+d = size(centroids, 1);
+
+% You need to return the following variables correctly.
+idx = zeros(size(X,1), 1);
+
+for i=1:length(idx) %para todos os m exemplos
+    for w=1:K % para todos os K centroides
+        d(w) = sqrt(sum((X(i,:)-centroids(w,:)).^2));
+    end
+    [M,I]=min(d);
+    idx(i) = I;
+end
+
+
+% =============================================================
+
+end
+
